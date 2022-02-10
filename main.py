2fafda056fa0b86b14619ce4547d5da25dfd73cf
dhokoon <52174999+dhokoon@users.noreply.github.com>
2022-02-09 16:10:00 -0800

    update
    utilized less code

--- main.py
+++ main.py
@@ -11,24 +11,21 @@
 
 
 # greeting
-print("Welcome to the tip calculator.")
+print("Welcome to the tip calculator.\n")
 
 # Get user input for total cost of bill
-bill = float(input("What was the total bill? $"))
+bill = float(input("What was the total bill?\n$ "))
 
 # calculate the cost of the tip
-tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+tip = float(input("What percentage tip would you like to give? 10, 12, or 15?\n "))
 
 # divide the total cost including tip amongst people in party
-split = int(input("How many people to split the bill? "))
+split = int(input("How many people to split the bill?\n "))
 
-tip_percent = tip / 100
-total_tip = bill * tip_percent
-total_bill = bill + total_tip
-bill_per_person = total_bill / split
-
-final_cost = round(bill_per_person, 2)
-final_cost = "{:.2f}".format(bill_per_person)
+bill_tip = bill * (1 + (tip / 100))
+total_split = bill_tip / float(split)
+sum = round(total_split, 2)
+sum = "{:.2f}".format(sum)
 
 # total of how much everyone owes
-print(f"Each person should pay: ${final_cost}")
+print(f"Each person should pay: ${sum}")
